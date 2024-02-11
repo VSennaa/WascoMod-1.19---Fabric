5d208aa4ad4a2602a90d24eb644576af27bbcf88
VSenna <contacriada.prabaixarapp@gmail.com>
2024-02-11 18:03:10 -0300

    Adicionado o escudo como item craftavel, embora ainda sem função de ser um escudo.

--- src/main/java/net/vsenna/wascomod/WascoMod.java
+++ src/main/java/net/vsenna/wascomod/WascoMod.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class WascoMod implements ModInitializer {
-	public static final String MOD_ID = "WascoMod";
+	public static final String MOD_ID = "wascomod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	//a
 	@Override
--- src/main/java/net/vsenna/wascomod/item/ModItems.java
+++ src/main/java/net/vsenna/wascomod/item/ModItems.java
@@ -1,23 +1,18 @@
 package net.vsenna.wascomod.item;
-
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.minecraft.item.Item;
-import net.minecraft.registry.Registry;
 import net.vsenna.wascomod.WascoMod;
 
 public class ModItems {
-    public static final Item MALTA_CROSS = registerItem("cross_malta", new Item(new FabricItemSettings().maxCount(1)));
 
-
-    private static Item registerItem(String name, Item item){
-        return Registry.register(Registries.ITEM, new Identifier(WascoMod.MOD_ID, name),item);
-    }
-
+    public static final Item MALTA_CROSS = new Item(new FabricItemSettings());
+    public static final Item WASCO_SHIELD = new Item(new FabricItemSettings());
     public static void registerModItems(){
         WascoMod.LOGGER.debug("Registering mod Items for " + WascoMod.MOD_ID);
+        Registry.register(Registries.ITEM, new Identifier(WascoMod.MOD_ID, "malta_cross"), MALTA_CROSS);
+        Registry.register(Registries.ITEM, new Identifier(WascoMod.MOD_ID, "wasco_shield"), WASCO_SHIELD);
     }
 }
