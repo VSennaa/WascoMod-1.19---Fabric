2ffe2ce5383f39b6d386d1ec87a2c62b79b58194
VSenna <contacriada.prabaixarapp@gmail.com>
2024-02-11 14:27:04 -0300

    initial

---
+++ src/main/java/net/vsenna/wascomod/WascoModClient.java
@@ -0,0 +1,10 @@
+package net.vsenna.wascomod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WascoModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
